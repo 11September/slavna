a740902979fb11a44da9d592bcba31be35c7988f
nosslsvec8 <nosiljevets@gmail.com>
2018-04-05 13:45:30 +0300

    add . folder, images

--- resources/views/partials/photo_report.blade.php
+++ resources/views/partials/photo_report.blade.php
@@ -3,8 +3,10 @@
 		<div class="photo_report_slider">
 		  <div>
 		  	<div class="row">
-		  		<div class="col-xl-6">1</div>
-		  		<div class="col-xl-6">2</div>
+		  		<div class="col-xl-6 block_text">1</div>
+		  		<div class="col-xl-6 block_img">
+		  			<img src="{{ asset('images/photo_report_slider_example.png') }}">
+		  		</div>
 		  	</div>
 		  </div>
 		  <div>your content</div>
