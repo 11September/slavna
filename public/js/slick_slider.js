fe38146dcf878abec64b70f59a1662915ed0ebe2
nosslsvec8 <nosiljevets@gmail.com>
2018-04-05 13:29:26 +0300

    fix add. slider

---
+++ public/js/slick_slider.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+  $('.photo_report_slider').slick({
+      initialSlide: 2,
+      slidesToShow: 1,
+      arrows: true,
+      dots: true,
+      infinite: false,
+      centerMode: true,
+      // vertical: true,
+      focusOnSelect: true,
+      responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: true,
+          dots: true,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          dots: false
+        }
+      }
+    ]
+  });
+
+});
--- resources/views/partials/photo_report.blade.php
+++ resources/views/partials/photo_report.blade.php
@@ -1,3 +1,9 @@
 <section class="photo_report">
-	
+	<div class="container">
+		<div class="photo_report_slider">
+		  <div>your content</div>
+		  <div>your content</div>
+		  <div>your content</div>
+		</div>
+	</div>
 </section>
