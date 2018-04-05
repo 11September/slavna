6385e729d7a436af2b3830205e3bb82738935b74
Vlasov977 <bovla1455@gmail.com>
2018-04-05 19:08:04 +0300

    my werstka

--- resources/views/partials/contactForm.blade.php
+++ resources/views/partials/contactForm.blade.php
@@ -1,3 +1,69 @@
 <section class="contactForm">
 
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="wrapper-text-left col-lg-6">
+                <h2 class="text-ua">Украина<br>
+                    Сумская область<br>
+                    Ахтырский район<br>
+                    с. Чернетчина<br>
+                    ул. Монастырська
+                </h2>
+
+                <p class="text-p-ua">
+                    (095) 65-02-810<br>
+                    (05446) 3-13-36<br>
+                    (098) 179-24-55<br>
+                    (прием банкетов - ресторан)<br><br>
+                    WWW.PARK-SLAVNA.COM.UA<br>
+                    PK_SLАVNА@UKR.NET<br>
+                </p>
+
+                <p class="my-fafa">
+
+                    <i class="fab fa-facebook-square "></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-google"></i>
+
+                </p>
+            </div>
+
+
+            <div class="wrapper-answer-form col-lg-6">
+
+                <h2 class="h2-text-quest">ЗАДАТЬ ВОПРОС </h2>
+                <HR>
+
+                <div class="input-special">
+                    <label for=" text">Имя*</label>
+                    <input type="text" placeholder="Имя">
+                </div>
+
+                <div class="input-special">
+
+                    <label for=" text">Телефон*</label>
+                    <input type="number">
+                </div>
+                    <div class="input-special">
+                        <label for=" text">E-mail*</label>
+                        <input type="text"  placeholder="E-mail*">
+                    </div>
+                  <textarea>
+                      Задайте свой вопрос
+                    </textarea>
+
+                <p class="under-form-text">
+                    Пожалуйста заполните все поля формы, и в скором времени наш<br>
+                    администратор ответит на ваши вопросы.
+                </p>
+
+                <span class="question-span">
+            Задать вопрос
+                </span>
+            </div>
+        </div>
+    </div>
+
+
 </section>
