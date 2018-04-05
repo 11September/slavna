1b136a9bc6bcba0bcf7dda3a92ddeb7329669ac1
nosslsvec8 <nosiljevets@gmail.com>
2018-04-05 16:37:14 +0300

    my 1920 verstka, section "Фотоотчет", страница "Главная"

--- public/js/slick_slider.js
+++ public/js/slick_slider.js
@@ -4,8 +4,10 @@ $(document).ready(function(){
       initialSlide: 0,
       slidesToShow: 1,
       rows: 1,
-      arrows: false,
+      arrows: true,
+      appendArrows: $('.photo_report_arrow'),
       dots: true,
+      appendDots: $('.photo_report_arrow'),
       infinite: false,
       centerMode: true,
       // vertical: true,
@@ -30,5 +32,28 @@ $(document).ready(function(){
       }
     ]
   });
+
+  $('.photo_report_slider_item').last().addClass("photo_report_slider_item_last");
+
+
+  var current_n = Number("0");
+  var last_n = Number("0");
+
+  $('.photo_report_slider_item').each(function(){
+
+    current = $('.photo_report_slider_item').attr("data-slick-index");
+    last_n = $('.photo_report_slider_item_last').attr("data-slick-index");
+
+    current = Number(current) + Number("1");
+    last_n = Number(last_n) + Number("1");
+
+
+    $('.current_number_slide').text("0" + current);
+    $('.all_number_slide').text("0" + last_n);
+
+    console.log( 0 + current );
+
+  });
+
 
 });
--- resources/views/partials/photo_report.blade.php
+++ resources/views/partials/photo_report.blade.php
@@ -1,16 +1,19 @@
 <section class="photo_report">
 	<div class="container">
 		<div class="photo_report_slider">
+
 		  <div class="photo_report_slider_item">
 		  	<div class="flexx">
 		  		<div class="block_text">
 		  			<h3>Фотоотчет с нового года 2018</h3>
 		  			<p>
-		  				Пусть в этот славный новогодний вечер, Сказка к вам волшебная придет. А Дед Мороз в своем мешке зветном Подарков в "Славну" непременно принесет :) Коттедж-парк "Славна" Ваш природных отдых в комфортных условиях.
+		  				Пусть в этот славный новогодний вечер, Сказка к вам волшебная придет. А Дед Мороз в своем мешке зветном Подарков в "Славну" непременно принесет <span>&#128516;</span> Коттедж-парк "Славна" Ваш природных отдых в комфортных условиях.
 		  			</p>
 		  			<div class="row photo_report_panel">
-		  				<div class="col-xl-6 photo_report_arrow">Дотс и стрелки</div>
-		  				<div class="col-xl-6 photo_report_link">
+		  				<div class="col-xl-4 photo_report_arrow">
+		  					<span class="current_number_slide"></span> <span>/</span> <span class="all_number_slide"></span>
+		  				</div>
+		  				<div class="col-xl-8 photo_report_link">
 		  					<a href="#">Подробней</a>
 		  				</div>
 		  			</div>
@@ -20,8 +23,95 @@
 		  		</div>
 		  	</div>
 		  </div>
-		  <div>your content</div>
-		  <div>your content</div>
+
+		  <div class="photo_report_slider_item">
+		  	<div class="flexx">
+		  		<div class="block_text">
+		  			<h3>Фотоотчет с нового года 2018</h3>
+		  			<p>
+		  				Пусть в этот славный новогодний вечер, Сказка к вам волшебная придет. А Дед Мороз в своем мешке зветном Подарков в "Славну" непременно принесет <span>&#128516;</span> Коттедж-парк "Славна" Ваш природных отдых в комфортных условиях.
+		  			</p>
+		  			<div class="row photo_report_panel">
+		  				<div class="col-xl-4 photo_report_arrow">
+		  					<span class="current_number_slide"></span> <span>/</span> <span class="all_number_slide"></span>
+		  				</div>
+		  				<div class="col-xl-8 photo_report_link">
+		  					<a href="#">Подробней</a>
+		  				</div>
+		  			</div>
+		  		</div>
+		  		<div class="block_img">
+		  			<img src="{{ asset('images/photo_report_slider_example.png') }}">
+		  		</div>
+		  	</div>
+		  </div>
+
+		  <div class="photo_report_slider_item">
+		  	<div class="flexx">
+		  		<div class="block_text">
+		  			<h3>Фотоотчет с нового года 2018</h3>
+		  			<p>
+		  				Пусть в этот славный новогодний вечер, Сказка к вам волшебная придет. А Дед Мороз в своем мешке зветном Подарков в "Славну" непременно принесет <span>&#128516;</span> Коттедж-парк "Славна" Ваш природных отдых в комфортных условиях.
+		  			</p>
+		  			<div class="row photo_report_panel">
+		  				<div class="col-xl-4 photo_report_arrow">
+		  					<span class="current_number_slide"></span> <span>/</span> <span class="all_number_slide"></span>
+		  				</div>
+		  				<div class="col-xl-8 photo_report_link">
+		  					<a href="#">Подробней</a>
+		  				</div>
+		  			</div>
+		  		</div>
+		  		<div class="block_img">
+		  			<img src="{{ asset('images/photo_report_slider_example.png') }}">
+		  		</div>
+		  	</div>
+		  </div>
+
+		  <div class="photo_report_slider_item">
+		  	<div class="flexx">
+		  		<div class="block_text">
+		  			<h3>Фотоотчет с нового года 2018</h3>
+		  			<p>
+		  				Пусть в этот славный новогодний вечер, Сказка к вам волшебная придет. А Дед Мороз в своем мешке зветном Подарков в "Славну" непременно принесет <span>&#128516;</span> Коттедж-парк "Славна" Ваш природных отдых в комфортных условиях.
+		  			</p>
+		  			<div class="row photo_report_panel">
+		  				<div class="col-xl-4 photo_report_arrow">
+		  					<span class="current_number_slide"></span> <span>/</span> <span class="all_number_slide"></span>
+		  				</div>
+		  				<div class="col-xl-8 photo_report_link">
+		  					<a href="#">Подробней</a>
+		  				</div>
+		  			</div>
+		  		</div>
+		  		<div class="block_img">
+		  			<img src="{{ asset('images/photo_report_slider_example.png') }}">
+		  		</div>
+		  	</div>
+		  </div>
+
+		  <div class="photo_report_slider_item">
+		  	<div class="flexx">
+		  		<div class="block_text">
+		  			<h3>Фотоотчет с нового года 2018</h3>
+		  			<p>
+		  				Пусть в этот славный новогодний вечер, Сказка к вам волшебная придет. А Дед Мороз в своем мешке зветном Подарков в "Славну" непременно принесет <span>&#128516;</span> Коттедж-парк "Славна" Ваш природных отдых в комфортных условиях.
+		  			</p>
+		  			<div class="row photo_report_panel">
+		  				<div class="col-xl-4 photo_report_arrow">
+		  					<span class="current_number_slide"></span> <span>/</span> <span class="all_number_slide"></span>
+		  				</div>
+		  				<div class="col-xl-8 photo_report_link">
+		  					<a href="#">Подробней</a>
+		  				</div>
+		  			</div>
+		  		</div>
+		  		<div class="block_img">
+		  			<img src="{{ asset('images/photo_report_slider_example.png') }}">
+		  		</div>
+		  	</div>
+		  </div>
+		  
 		</div>
 	</div>
 </section>
