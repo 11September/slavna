1df2983c9a73b6c2caba4ad8997d3c0879c8c14b
nosslsvec8 <nosiljevets@gmail.com>
2018-04-06 14:45:22 +0300

    add. struct my slider_3

---
+++ resources/views/partials/latest_publications.blade.php
@@ -0,0 +1,73 @@
+<section class="photo_report latest_publication">
+	<div class="container full_cr">
+		<div class="photo_report_slider">
+
+		  <div class="photo_report_slider_item">
+		  	<div class="flexx">
+		  		<div class="block_text">
+		  			<h3>Идея веселого выкупа невесты</h3>
+		  			<p>
+		  				Давно выяснено, что при оценке дизайна и композици читаемый текст мешает сосредоточиться. Lorem Ipsum используют потму, что тот обеспечиват более или менее стандартное заполнение шаблоа, а также реальное распределиние букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст.."
+		  			</p>
+		  			<div class="row photo_report_panel">
+		  				<div class="col-xl-4 photo_report_arrow">
+		  					<span class="current_number_slide"></span> <span>/</span> <span class="all_number_slide"></span>
+		  				</div>
+		  				<div class="col-xl-8 photo_report_link">
+		  					<a href="#">Подробней</a>
+		  				</div>
+		  			</div>
+		  		</div>
+		  		<div class="block_img">
+		  			<img src="{{ asset('images/latest_publication_slider_example.png') }}">
+		  		</div>
+		  	</div>
+		  </div>
+
+		  <div class="photo_report_slider_item">
+		  	<div class="flexx">
+		  		<div class="block_text">
+		  			<h3>Идея веселого выкупа невесты</h3>
+		  			<p>
+		  				Давно выяснено, что при оценке дизайна и композици читаемый текст мешает сосредоточиться. Lorem Ipsum используют потму, что тот обеспечиват более или менее стандартное заполнение шаблоа, а также реальное распределиние букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст.."
+		  			</p>
+		  			<div class="row photo_report_panel">
+		  				<div class="col-xl-4 photo_report_arrow">
+		  					<span class="current_number_slide"></span> <span>/</span> <span class="all_number_slide"></span>
+		  				</div>
+		  				<div class="col-xl-8 photo_report_link">
+		  					<a href="#">Подробней</a>
+		  				</div>
+		  			</div>
+		  		</div>
+		  		<div class="block_img">
+		  			<img src="{{ asset('images/latest_publication_slider_example.png') }}">
+		  		</div>
+		  	</div>
+		  </div>
+
+		  <div class="photo_report_slider_item">
+		  	<div class="flexx">
+		  		<div class="block_text">
+		  			<h3>Идея веселого выкупа невесты</h3>
+		  			<p>
+		  				Давно выяснено, что при оценке дизайна и композици читаемый текст мешает сосредоточиться. Lorem Ipsum используют потму, что тот обеспечиват более или менее стандартное заполнение шаблоа, а также реальное распределиние букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст.."
+		  			</p>
+		  			<div class="row photo_report_panel">
+		  				<div class="col-xl-4 photo_report_arrow">
+		  					<span class="current_number_slide"></span> <span>/</span> <span class="all_number_slide"></span>
+		  				</div>
+		  				<div class="col-xl-8 photo_report_link">
+		  					<a href="#">Подробней</a>
+		  				</div>
+		  			</div>
+		  		</div>
+		  		<div class="block_img">
+		  			<img src="{{ asset('images/latest_publication_slider_example.png') }}">
+		  		</div>
+		  	</div>
+		  </div>
+		  
+		</div>
+	</div>
+</section>
