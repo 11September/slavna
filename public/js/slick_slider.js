e05b54c2415cc9c518faa06b18d9f1cf36bc7b5c
nosslsvec8 <nosiljevets@gmail.com>
2018-04-06 14:29:15 +0300

    add . struct my slider_2

--- public/js/slick_slider.js
+++ public/js/slick_slider.js
@@ -44,13 +44,13 @@ $(document).ready(function(){
       initialSlide: 0,
       slidesToShow: 1,
       rows: 1,
-      arrows: false,
-      // appendArrows: $('.photo_report_arrow'),
+      arrows: true,
+      appendArrows: $('.clider_info'),
       dots: true,
-      // appendDots: $('.photo_report_arrow'),
+      appendDots: $('.clider_info'),
       infinite: false,
       centerMode: true,
-      // vertical: true,
+      vertical: true,
       focusOnSelect: true,
       responsive: [
       {
@@ -62,6 +62,22 @@ $(document).ready(function(){
         }
       }
     ]
+  });
+
+  $('.reviews_slider_item').last().addClass("reviews_slider_item_last");
+  var element = Number("1");
+
+  $('.reviews_slider_item').each(function(){
+
+    current = $('.reviews_slider_item').attr("data-slick-index");
+    last_n = $('.reviews_slider_item_last').attr("data-slick-index");
+
+    current = Number(current) + Number(element);
+    last_n = Number(last_n) + Number("1");
+    element = Number(element) + Number("1");
+
+    $(this).find('.reviews_current_number_slide').text("0" + current);
+    $('.reviews_all_number_slide').text("0" + last_n);
   });
 
 
--- resources/views/partials/reviews.blade.php
+++ resources/views/partials/reviews.blade.php
@@ -1,9 +1,172 @@
 <section class="reviews">
-	<div class="container full_cr">
-			<div class="reviews_slider">
-				<div>1</div>
-				<div>2</div>
-				<div>3</div>
+	<div class="container-fluid">
+		<div class="reviews_slider">
+
+			<div class="reviews_slider_item">
+					<div class="row row_head_info">
+						<div class="reviews_slider_img">
+							<img src="{{ asset('images/reviews_slider_example.png') }}">
+						</div>
+						<div class="reviews_slider_head">
+							<h2>Что о нас говорят?</h2>
+							<p>
+								Предлагаем вам прочесть отзывы наших гостей, мы будем очень рады, если вы оставите свой.
+							</p>
+						</div>
+					</div>
+					<div class="row row_content_slider">
+						<div class="slider_bg">
+							<p>
+								"Идеальное место, чтоб уехать из город, что мы собственно и сделали. Сервис на высоте, атмосфера как дома, очень понравилось и домой ехать не хотелось. Спасибо персоналу за вежливость и позитив)"
+							</p>
+						</div>
+						<div class="clider_info">
+							<span class="reviews_current_number_slide"></span>
+							<span>_</span>
+							<span class="reviews_all_number_slide"></span>
+						</div>
+					</div>
+					<div class="row row_links">
+						<a class="add_reviews" href="#">Добавить отзыв</a>
+						<a class="all_reviews" href="#">Читать все отзывы</a>
+					</div>
+					<div class="row row_line">
+						<img src="">
+					</div>
 			</div>
+
+			<div class="reviews_slider_item">
+					<div class="row row_head_info">
+						<div class="reviews_slider_img">
+							<img src="{{ asset('images/reviews_slider_example.png') }}">
+						</div>
+						<div class="reviews_slider_head">
+							<h2>Что о нас говорят?</h2>
+							<p>
+								Предлагаем вам прочесть отзывы наших гостей, мы будем очень рады, если вы оставите свой.
+							</p>
+						</div>
+					</div>
+					<div class="row row_content_slider">
+						<div class="slider_bg">
+							<p>
+								"Идеальное место, чтоб уехать из город, что мы собственно и сделали. Сервис на высоте, атмосфера как дома, очень понравилось и домой ехать не хотелось. Спасибо персоналу за вежливость и позитив)"
+							</p>
+						</div>
+						<div class="clider_info">
+							<span class="current_number_slidee"></span>
+							<span>_</span>
+							<span class="all_number_slidee"></span>
+						</div>
+					</div>
+					<div class="row row_links">
+						<a href="#">Добавить отзыв</a>
+						<a href="#">Читать все отзывы</a>
+					</div>
+					<div class="row row_line">
+						<img src="">
+					</div>
+			</div>
+
+			<div class="reviews_slider_item">
+					<div class="row row_head_info">
+						<div class="reviews_slider_img">
+							<img src="{{ asset('images/reviews_slider_example.png') }}">
+						</div>
+						<div class="reviews_slider_head">
+							<h2>Что о нас говорят?</h2>
+							<p>
+								Предлагаем вам прочесть отзывы наших гостей, мы будем очень рады, если вы оставите свой.
+							</p>
+						</div>
+					</div>
+					<div class="row row_content_slider">
+						<div class="slider_bg">
+							<p>
+								"Идеальное место, чтоб уехать из город, что мы собственно и сделали. Сервис на высоте, атмосфера как дома, очень понравилось и домой ехать не хотелось. Спасибо персоналу за вежливость и позитив)"
+							</p>
+						</div>
+						<div class="clider_info">
+							<span class="current_number_slidee"></span>
+							<span>_</span>
+							<span class="all_number_slidee"></span>
+						</div>
+					</div>
+					<div class="row row_links">
+						<a href="#">Добавить отзыв</a>
+						<a href="#">Читать все отзывы</a>
+					</div>
+					<div class="row row_line">
+						<img src="">
+					</div>
+			</div>
+
+			<div class="reviews_slider_item">
+					<div class="row row_head_info">
+						<div class="reviews_slider_img">
+							<img src="{{ asset('images/reviews_slider_example.png') }}">
+						</div>
+						<div class="reviews_slider_head">
+							<h2>Что о нас говорят?</h2>
+							<p>
+								Предлагаем вам прочесть отзывы наших гостей, мы будем очень рады, если вы оставите свой.
+							</p>
+						</div>
+					</div>
+					<div class="row row_content_slider">
+						<div class="slider_bg">
+							<p>
+								"Идеальное место, чтоб уехать из город, что мы собственно и сделали. Сервис на высоте, атмосфера как дома, очень понравилось и домой ехать не хотелось. Спасибо персоналу за вежливость и позитив)"
+							</p>
+						</div>
+						<div class="clider_info">
+							<span class="current_number_slidee"></span>
+							<span>_</span>
+							<span class="all_number_slidee"></span>
+						</div>
+					</div>
+					<div class="row row_links">
+						<a class="add_reviews" href="#">Добавить отзыв</a>
+						<a class="all_reviews" href="#">Читать все отзывы</a>
+					</div>
+					<div class="row row_line">
+						<img src="">
+					</div>
+			</div>
+
+			<div class="reviews_slider_item">
+					<div class="row row_head_info">
+						<div class="reviews_slider_img">
+							<img src="{{ asset('images/reviews_slider_example.png') }}">
+						</div>
+						<div class="reviews_slider_head">
+							<h2>Что о нас говорят?</h2>
+							<p>
+								Предлагаем вам прочесть отзывы наших гостей, мы будем очень рады, если вы оставите свой.
+							</p>
+						</div>
+					</div>
+					<div class="row row_content_slider">
+						<div class="slider_bg">
+							<p>
+								"Идеальное место, чтоб уехать из город, что мы собственно и сделали. Сервис на высоте, атмосфера как дома, очень понравилось и домой ехать не хотелось. Спасибо персоналу за вежливость и позитив)"
+							</p>
+						</div>
+						<div class="clider_info">
+							<span class="current_number_slidee"></span>
+							<span>_</span>
+							<span class="all_number_slidee"></span>
+						</div>
+					</div>
+					<div class="row row_links">
+						<a class="add_reviews" href="#">Добавить отзыв</a>
+						<a class="all_reviews" href="#">Читать все отзывы</a>
+					</div>
+					<div class="row row_line">
+						<img src="">
+					</div>
+			</div>
+
+		</div>
 	</div>
 </section>
