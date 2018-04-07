71ec23c745a50471c07fbf25316fc946a8c83740
Vlasov977 <bovla1455@gmail.com>
2018-04-07 18:07:35 +0300

    fix section form

---
+++ resources/views/all-photos.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('css')
+
+@endsection
+
+@section('content')
+    @include('partials.photo-items')
+    @include('partials.contactForm')
+
+@endsection
+
+@section('scripts')
+
+@endsection
---
+++ resources/views/partials/photo-items.blade.php
@@ -0,0 +1,76 @@
+<section class="photo-items">
+    <div class="container">
+
+        <div class="row">
+
+            <div class="photo-left col-lg-6">
+                <img class="twest-1" src="images/1.png" alt="">
+
+            </div>
+
+            <div class="photo-right col-lg-6">
+                <img src="images/2.png" alt="">
+            </div>
+
+        </div>
+
+
+        <div class="row">
+
+            <div class="photo-left col-lg-6">
+                <img src="images/3.png" alt="">
+            </div>
+
+            <div class="photo-right col-lg-6">
+                <img src="images/4.png" alt="">
+            </div>
+
+        </div>
+
+
+        <div class="row">
+
+            <div class="photo-left col-lg-6">
+                <img src="images/5.png" alt="">
+            </div>
+
+            <div class="photo-right col-lg-6">
+                <img src="images/6.png" alt="">
+            </div>
+
+        </div>
+
+
+        <div class="row">
+
+            <div class="photo-left col-lg-6">
+                <img src="images/7.png" alt="">
+            </div>
+
+            <div class="photo-right col-lg-6">
+                <img src="images/8.png" alt="">
+            </div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="photo-left col-lg-6">
+                <img src="images/9.png" alt="">
+            </div>
+
+            <div class="photo-right col-lg-6">
+                <img src="images/10.png" alt="">
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+    </div>
+</section>
