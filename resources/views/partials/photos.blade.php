8900941548522d71b43827c20b8d5214bd31bff5
YuraBoyco <boyco-yura@yandex.ru>
2018-04-09 16:28:01 +0300

    add short-about to project

---
+++ resources/views/partials/photos.blade.php
@@ -0,0 +1,30 @@
+<section class="photos-in-album">
+
+    <p class="albom-header col-lg-12">Альбом "Достижения"</p>
+
+    <div class="all-photos row">
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <img src="{{ asset('images/image-in-album.png') }}" alt="">
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <img src="{{ asset('images/image-in-album.png') }}" alt="">
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <img src="{{ asset('images/image-in-album.png') }}" alt="">
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <img src="{{ asset('images/image-in-album.png') }}" alt="">
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <img src="{{ asset('images/image-in-album.png') }}" alt="">
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <img src="{{ asset('images/image-in-album.png') }}" alt="">
+        </div>
+    </div>
+</section>
