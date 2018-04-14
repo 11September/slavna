c5ca9e305a44f77ff2beeb3539475773e4863393
Stanislav <shiptya@icloud.com>
2018-04-14 20:09:40 +0300

    ** Clear space

--- resources/views/partials/album-items.blade.php
+++ resources/views/partials/album-items.blade.php
@@ -27,50 +27,6 @@
 
             @endforeach
 
-
-
-            {{--<div class="photo-left col-lg-6">--}}
-            {{--<img class="twest-1" src="images/3.png" alt="">--}}
-            {{--<p class="hover-item-text">Украшение залов</p>--}}
-            {{--</div>--}}
-
-            {{--<div class="photo-right col-lg-6">--}}
-            {{--<img class="twest-1" src="images/4.png" alt="">--}}
-            {{--<p class="hover-item-text">Украшение залов</p>--}}
-            {{--</div>--}}
-
-
-            {{--<div class="photo-left col-lg-6">--}}
-            {{--<img class="twest-1" src="images/5.png" alt="">--}}
-            {{--<p class="hover-item-text">Украшение залов</p>--}}
-            {{--</div>--}}
-
-            {{--<div class="photo-right col-lg-6">--}}
-            {{--<img class="twest-1" src="images/6.png" alt="">--}}
-            {{--<p class="hover-item-text">Украшение залов</p>--}}
-            {{--</div>--}}
-
-            {{--<div class="photo-left col-lg-6">--}}
-            {{--<img class="twest-1" src="images/7.png" alt="">--}}
-            {{--<p class="hover-item-text">Украшение залов</p>--}}
-            {{--</div>--}}
-
-            {{--<div class="photo-right col-lg-6">--}}
-            {{--<img class="twest-1" src="images/8.png" alt="">--}}
-            {{--<p class="hover-item-text">Украшение залов</p>--}}
-            {{--</div>--}}
-
-            {{--<div class="photo-left col-lg-6">--}}
-            {{--<img class="twest-1" src="images/9.png" alt="">--}}
-            {{--<p class="hover-item-text">Украшение залов</p>--}}
-            {{--</div>--}}
-
-            {{--<div class="photo-right col-lg-6">--}}
-            {{--<img class="twest-1" src="images/10.png" alt="">--}}
-            {{--<p class="hover-item-text">Украшение залов</p>--}}
-            {{--</div>--}}
-
         </div>
-
     </div>
 </section>
