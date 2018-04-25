bd1225ca0cd9342639166202488cbfdba5b28081
Stanislav <shiptya@icloud.com>
2018-04-25 17:13:55 +0300

    ** Fix page video

--- resources/views/partials/video.blade.php
+++ resources/views/partials/video.blade.php
@@ -4,26 +4,12 @@
         <div class="row">
 
             @foreach($videos as $video)
-                <div class="photo-left col-lg-12">
+                <div class="photo-left col-lg-6">
                     <div class="wrapper-twest-hover">
                         <iframe width="560" height="315" src="{{ $video->src }}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                        <p class="hover-item-text">Украшение залов</p>
                     </div>
                 </div>
             @endforeach
-
-            {{--<div class="photo-left col-lg-12">--}}
-                {{--<div class="wrapper-twest-hover">--}}
-                    {{--<video width="100%" height="100%" controls="controls" poster="images/1.png">--}}
-                       {{--<source src="video/duel.ogv" type='video/ogg; codecs="theora, vorbis"'>--}}
-                       {{--<source src="video/duel.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>--}}
-                       {{--<source src="video/duel.webm" type='video/webm; codecs="vp8, vorbis"'>--}}
-                       {{--Тег video не поддерживается вашим браузером. --}}
-                       {{--<a href="video/duel.mp4">Скачайте видео</a>.--}}
-                    {{--</video>--}}
-                    {{--<p class="hover-item-text">Украшение залов</p>--}}
-                {{--</div>--}}
-            {{--</div>--}}
 
         </div>
 
