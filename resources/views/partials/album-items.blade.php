8df454388890f8b4091ada850cf400fee5191953
YuraBoyco <boyco-yura@yandex.ru>
2018-04-26 18:41:07 +0300

    fixik

--- resources/views/partials/album-items.blade.php
+++ resources/views/partials/album-items.blade.php
@@ -7,21 +7,25 @@
 
                 @if ($loop->iteration % 2)
                     <div class="photo-right col-lg-6">
-                        <img class="twest-1" src="{{ asset('storage/' . $album->image) }}" alt="">
-                        <p class="hover-item-text">
-                            <a href="{{ action('PagesController@photosAlbum', $album->id) }}">
-                                {{ $album->name }}
-                            </a>
-                        </p>
+                        <div class="grid-fix">
+                            <img class="twest-1" src="{{ asset('storage/' . $album->image) }}" alt="">
+                            <p class="hover-item-text">
+                                <a href="{{ action('PagesController@photosAlbum', $album->id) }}">
+                                    {{ $album->name }}
+                                </a>
+                            </p>
+                        </div>
                     </div>
                 @else
                     <div class="photo-left col-lg-6">
-                        <img class="twest-1" src="{{ asset('storage/' . $album->image) }}" alt="">
-                        <p class="hover-item-text">
-                            <a href="{{ action('PagesController@photosAlbum', $album->id) }}">
-                                {{ $album->name }}
-                            </a>
-                        </p>
+                        <div class="grid-fix">
+                            <img class="twest-1" src="{{ asset('storage/' . $album->image) }}" alt="">
+                            <p class="hover-item-text">
+                                <a href="{{ action('PagesController@photosAlbum', $album->id) }}">
+                                    {{ $album->name }}
+                                </a>
+                            </p>
+                        </div>
                     </div>
                 @endif
 
