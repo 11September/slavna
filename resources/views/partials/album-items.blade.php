5b3f8502a5c0ce9b090890220e45d8d4d1b1c5eb
YuraBoyco <boyco-yura@yandex.ru>
2018-04-26 18:51:47 +0300

    fix image album

--- resources/views/partials/album-items.blade.php
+++ resources/views/partials/album-items.blade.php
@@ -8,7 +8,7 @@
                 @if ($loop->iteration % 2)
                     <div class="photo-right col-lg-6">
                         <div class="grid-fix">
-                            <img class="twest-1" src="{{ asset('storage/' . $album->image) }}" alt="">
+                            <a class="album-link twest-1" href="{{ action('PagesController@photosAlbum', $album->id) }}"><img class="twest-1" src="{{ asset('storage/' . $album->image) }}" alt=""></a>
                             <p class="hover-item-text">
                                 <a href="{{ action('PagesController@photosAlbum', $album->id) }}">
                                     {{ $album->name }}
@@ -19,7 +19,7 @@
                 @else
                     <div class="photo-left col-lg-6">
                         <div class="grid-fix">
-                            <img class="twest-1" src="{{ asset('storage/' . $album->image) }}" alt="">
+                            <a class="album-link twest-1" href="{{ action('PagesController@photosAlbum', $album->id) }}"><img class="twest-1" src="{{ asset('storage/' . $album->image) }}" alt=""></a>
                             <p class="hover-item-text">
                                 <a href="{{ action('PagesController@photosAlbum', $album->id) }}">
                                     {{ $album->name }}
